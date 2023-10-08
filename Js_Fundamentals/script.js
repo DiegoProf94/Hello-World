4ed9c1cbe2afc9298a0787a04717eec13bd55c79
Diego <diego.prof94@gmail.com>
2023-10-08 18:33:41 -0300

    Data types info

--- Js_Fundamentals/script.js
+++ Js_Fundamentals/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+// Variables
+// Name must contain only letter, digits, symbols $ and _
+// First character must not be a digit
+// Usualy "camelcase" style for naming; uppercase for consts
+
+let fruit = 'Apple';
+var animal = 'Dog';
+const person = 'Diego';
+
+//Data types
+
+let number = 123456;
+let bigint = 10n;
+let string = 'Hi, nice to meet you';
+let boolean = true;
+let object = {};
+let symbol = Symbol();
+let Null = null;
+let Undefined = undefined;
